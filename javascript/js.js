91527126a1f556bd54e065219204acf8d94bdb65
Kevin Tu <kevin.b.tu@gmail.com>
2014-06-10 07:52:13 -0700

    added progress bar on landing page

--- javascript/js.js
+++ javascript/js.js
@@ -1,6 +1,25 @@
 // Loading Page
+
 $(window).load(function() {
-    window.setTimeout(hideLanding,1000)
+    // progress bar animation
+    var bar = document.getElementById('progress-bar'),
+        fallback = document.getElementById('download-progress'),
+        loaded = 0;
+    var load = function() {
+        loaded += Math.floor((Math.random() * 10) + 1);
+        bar.value = loaded;
+        $(fallback).empty().append("HTML5 progress tag not supported: ");
+        if (loaded == 100) {
+            clearInterval(beginLoad);
+            console.log('Load was performed.');
+        }
+    };
+
+    var beginLoad = setInterval(function() {
+        load();
+    }, 50);
+    // Set extra timeout for loading page
+    window.setTimeout(hideLanding,1000);
   });
 function hideLanding(){
     $(this).scrollTop(0);
