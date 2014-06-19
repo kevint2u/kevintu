399e4dc68f2d64c3d98d0244fb96796c54d83826
Kevin Tu <kevin.b.tu@gmail.com>
2014-06-19 16:56:58 -0700

    js for fading in place. still need to fix!

--- javascript/index.js
+++ javascript/index.js
@@ -50,7 +50,23 @@ $(document).ready(function(){
     });    
 });
 
+// Fading of logo
+var fadeStart=100 // 100px scroll or less will equiv to 1 opacity
+    ,fadeUntil=200 // 200px scroll or more will equiv to 0 opacity
+    ,fading = $('#loading-wrapper')
+;
 
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=0
+    ;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    fading.css('opacity',opacity);
+});
 // Create HTML5 elements for IE
   
 document.createElement("article");
