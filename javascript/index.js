f20a3781990d573306ecec1ee3f4753c1dc94523
Kevin Tu <kevin.b.tu@gmail.com>
2016-06-14 10:35:24 -0700

    added scrollify base code

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,23 @@
 $(window).load(function() {
     $(this).scrollTop(0);
-    // progress bar animation
-    var bar = document.getElementById('progress-bar'),
-        fallback = document.getElementById('download-progress'),
-        loaded = 0;
-    var load = function() {
-        loaded += Math.floor((Math.random() * 2) + 3);
-        bar.value = loaded;
-        $(fallback).empty().append("HTML5 progress tag not supported: ");
-        if (loaded == 100) {
-            clearInterval(beginLoad);
-            console.log('Load was performed.');
-        }
-    };
 
-    var beginLoad = setInterval(function() {
-        load();
-    }, 10);
-    // Set extra timeout for loading page
-    $(this).scrollTop(0);
-    window.setTimeout(hideLanding,1750);
-  });
-function hideLanding(){
-    $(this).scrollTop(0);
-    $('#loading').fadeOut(500);
-}
+    // Setup Scrollify
+    $(function() {
+        $.scrollify({
+            section : ".panel",
+            sectionName : "section-name",
+            interstitialSection : "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset : 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            before:function() {},
+            after:function() {},
+            afterResize:function() {},
+            afterRender:function() {}
+        });
+    });
+
+});
