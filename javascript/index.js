7caa8e5c326bbc730ca5a90309ca98aa3d975b5f
Kevin Tu <kevin.b.tu@gmail.com>
2016-06-13 21:14:29 -0700

    pushing temp landing page

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
+""" Set temporary landing page """
+window.location.href='temporary.html'
+
 $(window).load(function() {
     $(this).scrollTop(0);
     // progress bar animation
@@ -24,8 +27,6 @@ $(window).load(function() {
 function hideLanding(){
     $(this).scrollTop(0);
     $('#loading').fadeOut(500);
-    """ Set temporary landing page """
-    window.location.href='temporary.html'
 }
 
 // Parallax Scrolling
